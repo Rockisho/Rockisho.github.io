3df255fe166e4c83fe20fa0b7529897cf88e6a8a
Rockisho <104766987+Rockisho@users.noreply.github.com>
2024-12-03 14:55:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+if ('ServiceWorker' in navigator){
+    navigator.serviceWorker.register('/service-worker.js')
+       .then(registration => console.log('Service Worker Registrado no Domínio: ', registration.scope))
+       .catch(err => console.log('O Registro do Service Worker Falhou: ', err));
+}
