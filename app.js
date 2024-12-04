dad84d24059837a7ef6a1dc8de93374d88b8b853
Rockisho <104766987+Rockisho@users.noreply.github.com>
2024-12-04 14:41:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ if ('ServiceWorker' in navigator){
        .then(registration => console.log('Service Worker Registrado no Domínio: ', registration.scope))
        .catch(err => console.log('O Registro do Service Worker Falhou: ', err));
 }
+
+// Inserir valor no display
+function inserir(valor) {
+    const resultado = document.getElementById("resultado");
+    resultado.value += valor;
+  }
+  
+  // Limpar o display
+  function limpar() {
+    document.getElementById("resultado").value = "";
+  }
+  
+  // Deletar o último caractere
+  function deletarUltimo() {
+    const resultado = document.getElementById("resultado");
+    resultado.value = resultado.value.slice(0, -1);
+  }
+  
+  // Calcular o resultado
+  function calcular() {
+    try {
+      const resultado = document.getElementById("resultado");
+      resultado.value = eval(resultado.value);
+    } catch {
+      alert("Erro ao calcular! Verifique a expressão.");
+    }
+  }  
